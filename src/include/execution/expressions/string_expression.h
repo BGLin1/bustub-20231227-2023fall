249bcd94afc23da328c2dcc96c4bf5c5e958fbc3
BGLin <576062010@qq.com>
2024-07-23 02:46:25 +0800

    完成project0

--- src/include/execution/expressions/string_expression.h
+++ src/include/execution/expressions/string_expression.h
@@ -30,64 +30,77 @@
 
 namespace bustub {
 
-enum class StringExpressionType { Lower, Upper };
+  enum class StringExpressionType { Lower, Upper };
 
-/**
- * StringExpression represents two expressions being computed.
- */
-class StringExpression : public AbstractExpression {
- public:
-  StringExpression(AbstractExpressionRef arg, StringExpressionType expr_type)
-      : AbstractExpression({std::move(arg)}, TypeId::VARCHAR), expr_type_{expr_type} {
-    if (GetChildAt(0)->GetReturnType() != TypeId::VARCHAR) {
-      throw bustub::NotImplementedException("expect the first arg to be varchar");
+  /**
+   * StringExpression represents two expressions being computed.
+   */
+  class StringExpression : public AbstractExpression {
+  public:
+    StringExpression(AbstractExpressionRef arg, StringExpressionType expr_type)
+      : AbstractExpression({ std::move(arg) }, TypeId::VARCHAR), expr_type_{ expr_type } {
+      if (GetChildAt(0)->GetReturnType() != TypeId::VARCHAR) {
+        throw bustub::NotImplementedException("expect the first arg to be varchar");
+      }
     }
-  }
 
-  auto Compute(const std::string &val) const -> std::string {
-    // TODO(student): implement upper / lower.
+    auto Compute(const std::string& val) const -> std::string {
+      switch (expr_type_) {
+      case StringExpressionType::Upper: {
+        std::string upper;
+        std::transform(val.begin(), val.end(), std::back_inserter(upper), ::toupper);
+        return upper;
+      }
+      case StringExpressionType::Lower: {
+        std::string lower;
+        std::transform(val.begin(), val.end(), std::back_inserter(lower), ::tolower);
+        return lower;
+      }
+      default:
+        BUSTUB_ASSERT(false, "unknown string expression type");
+    }
     return {};
-  }
+    }
 
-  auto Evaluate(const Tuple *tuple, const Schema &schema) const -> Value override {
-    Value val = GetChildAt(0)->Evaluate(tuple, schema);
-    auto str = val.GetAs<char *>();
-    return ValueFactory::GetVarcharValue(Compute(str));
-  }
+    auto Evaluate(const Tuple* tuple, const Schema& schema) const -> Value override {
+      Value val = GetChildAt(0)->Evaluate(tuple, schema);
+      auto str = val.GetAs<char*>();
+      return ValueFactory::GetVarcharValue(Compute(str));
+    }
 
-  auto EvaluateJoin(const Tuple *left_tuple, const Schema &left_schema, const Tuple *right_tuple,
-                    const Schema &right_schema) const -> Value override {
-    Value val = GetChildAt(0)->EvaluateJoin(left_tuple, left_schema, right_tuple, right_schema);
-    auto str = val.GetAs<char *>();
-    return ValueFactory::GetVarcharValue(Compute(str));
-  }
+    auto EvaluateJoin(const Tuple* left_tuple, const Schema& left_schema, const Tuple* right_tuple,
+      const Schema& right_schema) const -> Value override {
+      Value val = GetChildAt(0)->EvaluateJoin(left_tuple, left_schema, right_tuple, right_schema);
+      auto str = val.GetAs<char*>();
+      return ValueFactory::GetVarcharValue(Compute(str));
+    }
 
-  /** @return the string representation of the expression node and its children */
-  auto ToString() const -> std::string override { return fmt::format("{}({})", expr_type_, *GetChildAt(0)); }
+    /** @return the string representation of the expression node and its children */
+    auto ToString() const -> std::string override { return fmt::format("{}({})", expr_type_, *GetChildAt(0)); }
 
-  BUSTUB_EXPR_CLONE_WITH_CHILDREN(StringExpression);
+    BUSTUB_EXPR_CLONE_WITH_CHILDREN(StringExpression);
 
-  StringExpressionType expr_type_;
+    StringExpressionType expr_type_;
 
- private:
-};
+  private:
+  };
 }  // namespace bustub
 
 template <>
 struct fmt::formatter<bustub::StringExpressionType> : formatter<string_view> {
   template <typename FormatContext>
-  auto format(bustub::StringExpressionType c, FormatContext &ctx) const {
+  auto format(bustub::StringExpressionType c, FormatContext& ctx) const {
     string_view name;
     switch (c) {
-      case bustub::StringExpressionType::Upper:
-        name = "upper";
-        break;
-      case bustub::StringExpressionType::Lower:
-        name = "lower";
-        break;
-      default:
-        name = "Unknown";
-        break;
+    case bustub::StringExpressionType::Upper:
+      name = "upper";
+      break;
+    case bustub::StringExpressionType::Lower:
+      name = "lower";
+      break;
+    default:
+      name = "Unknown";
+      break;
     }
     return formatter<string_view>::format(name, ctx);
   }
--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -1,41 +1,46 @@
 #include "primer/trie.h"
 #include <string_view>
 #include "common/exception.h"
-
+#include <stack>
 namespace bustub {
 
   template <class T>
   auto Trie::Get(std::string_view key) const -> const T* {
-    throw NotImplementedException("Trie::Get is not implemented.");
+
 
     // You should walk through the trie to find the node corresponding to the key. If the node doesn't exist, return
     // nullptr. After you find the node, you should use `dynamic_cast` to cast it to `const TrieNodeWithValue<T> *`. If
     // dynamic_cast returns `nullptr`, it means the type of the value is mismatched, and you should return nullptr.
     // Otherwise, return the value.
 
-    if (key.empty()) {
-      return nullptr;
-    }
     //根节点
     auto c_root = this->root_;
 
-    for (auto c : key) {
+    for (auto& c : key) {
+      if (c_root == nullptr) {
+        std::cout << "get key: " << key << " failure" << std::endl;
+        return nullptr;
+      }
       auto iter = c_root->children_.find(c);
       if (iter != c_root->children_.end()) {
-        c_root = iter->second;
+        c_root = std::const_pointer_cast<TrieNode>(iter->second);
       } else {
+        std::cout << "get key: " << key << " failure" << std::endl;
         return nullptr;
       }
     }
 
     if (c_root == nullptr || !c_root->is_value_node_) {
+      std::cout << "get key: " << key << " failure" << std::endl;
       return nullptr;
     }
     //找到了
     auto c_root_with_value = dynamic_cast<const TrieNodeWithValue<T> *>(c_root.get());
     if (c_root_with_value) {
+      std::cout << "get key: " << key << std::endl;
       return c_root_with_value->value_.get();
     } else {
+      std::cout << "get key: " << key << " failure" << std::endl;
       return nullptr;
     }
   }
@@ -43,38 +48,49 @@ namespace bustub {
   template <class T>
   auto Trie::Put(std::string_view key, T value) const -> Trie {
     // Note that `T` might be a non-copyable type. Always use `std::move` when creating `shared_ptr` on that value.
-    throw NotImplementedException("Trie::Put is not implemented.");
+
 
     // You should walk through the trie and create new nodes if necessary. If the node corresponding to the key already
     // exists, you should create a new `TrieNodeWithValue`.
+
+
+    //只要调用此接口 就一定会生成一个新的root
+
+    //key为空就是直接取代根节点中
     if (key.empty()) {
-      return Trie(nullptr);
+      auto new_root = root_ ? std::make_shared<TrieNodeWithValue<T>>(root_->children_, std::make_shared<T>(std::move
+      (value))) : std::make_shared<TrieNodeWithValue<T>>(std::make_shared<T>(std::move
+      (value)));
+      std::cout << "Put:" << " key:" << key << std::endl;
+      return Trie(new_root);
     }
 
-    //只要有新的值需要插入 就一定会生成一个新的root
     auto new_root = this->root_ ? std::shared_ptr<TrieNode>(this->root_->Clone()) : std::make_shared<TrieNode>();
-
     auto c_root = new_root;
-    for (int i = 0;i < key.size() - 1;i++) {
+    for (unsigned int i = 0;i < key.size() - 1;i++) {
       char c = key[i];
-      auto iter = c_root->children_.find(c);
-      //节点存在
+
       if (auto iter = c_root->children_.find(c); iter != c_root->children_.end()) {
-        c_root->children_[c] = iter->second->Clone();
+        //节点存在
+        c_root->children_[c] = std::shared_ptr<TrieNode>(iter->second->Clone());
       } else {
-        c_root->children_[c] = std::make_shared<TrieNode>();//
+        c_root->children_[c] = std::make_shared<TrieNode>();
       }
       //指针下移
       c_root = std::const_pointer_cast<TrieNode>(c_root->children_[c]);
     }
     //最后一个节点变成一个值节点
     char cc = key[key.size() - 1];
-    if (auto iter = c_root->children_.find(key[cc]); iter == c_root->children_.end()) {
-      //最后一个节点不存在
-      c_root->children_[cc] = std::make_shared<TrieNodeWithValue<T>>(std::make_shared<T>(std::move(value)));
-    } else {
+
+
+    if (auto iter = c_root->children_.find(cc); iter != c_root->children_.end()) {
       //最后一个节点原本存在
       c_root->children_[cc] = std::make_shared<TrieNodeWithValue<T>>(c_root->children_[cc]->children_, std::make_shared<T>(std::move(value)));
+      std::cout << "Put:" << "replace key:" << key << std::endl;
+    } else {
+      //最后一个节点不存在
+      c_root->children_[cc] = std::make_shared<TrieNodeWithValue<T>>(std::make_shared<T>(std::move(value)));
+      std::cout << "Put:" << "create key:" << key << std::endl;
     }
     return Trie(new_root);
   }
@@ -82,17 +98,25 @@ namespace bustub {
   // You should walk through the trie and remove nodes if necessary. If the node doesn't contain a value any more,
   // you should convert it to `TrieNode`. If a node doesn't have children any more, you should remove it.
   auto Trie::Remove(std::string_view key) const -> Trie {
-    throw NotImplementedException("Trie::Remove is not implemented.");
+
+
     if (key.empty()) {
-      return Trie(nullptr);
+      if (this->root_) {
+        auto new_root = std::make_shared<TrieNode>(root_->children_);
+        return Trie(new_root);
+      } else {
+        return Trie(std::make_shared<TrieNode>());
+      }
     }
     //只要有值删除 就一定会生成一个新的root
     auto new_root = this->root_ ? std::shared_ptr<TrieNode>(this->root_->Clone()) : std::make_shared<TrieNode>();
     auto c_root = new_root;
+    //把路径上所有经过的节点入栈
+    auto node_stack = std::make_unique<std::stack<std::shared_ptr<TrieNode>>>();
+    node_stack->push(c_root);
+    for (unsigned int i = 0;i < key.size() - 1;i++) {
 
-    for (int i = 0;i < key.size() - 1;i++) {
       char c = key[i];
-      auto iter = c_root->children_.find(c);
       //节点存在
       if (auto iter = c_root->children_.find(c); iter != c_root->children_.end()) {
         c_root->children_[c] = iter->second->Clone();
@@ -103,11 +127,13 @@ namespace bustub {
       }
       //指针下移
       c_root = std::const_pointer_cast<TrieNode>(c_root->children_[c]);
+      node_stack->push(c_root);
     }
     //遍历到了最后一个节点
     char c = key[key.size() - 1];
-    if (auto iter = c_root->children_.find(key[c]); iter == c_root->children_.end()) {
+    if (auto iter = c_root->children_.find(c); iter == c_root->children_.end()) {
       //最后一个节点不存在
+      std::cerr << "Trie::Remove() : can not find key :" << key << std::endl;
       return Trie(new_root);
     } else {
       //最后一个节点存在
@@ -117,7 +143,24 @@ namespace bustub {
       } else {
         //最后一个节点没有子节点
         c_root->children_.erase(c);
+        std::cout << "Remove:" << "delete key:" << key << std::endl;
       }
+    }
+    //遍历栈中的所有节点查看哪一个非值节点没有子节点 找到就把这个节点也删除 
+    for (int i = key.size() - 2;i >= 0;i--) {
+      auto c_node = node_stack->top();
+      node_stack->pop();
+      if (!c_node->is_value_node_ && c_node->children_.empty()) {
+        auto father_node = node_stack->top();
+        father_node->children_.erase(key[i]);
+      } else {
+        break;
+      }
+    }
+    //最后判断根节点是不是空
+    if (new_root->children_.empty()) {
+      std::cout<<"Remove:"<<"delete root"<<std::endl;
+      return Trie(nullptr);
     }
     return Trie(new_root);
   }
